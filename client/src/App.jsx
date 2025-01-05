61d9cdd9774c130baefd3ccb66e8e8a82a48612b
Kinkin <hiimkien2702@gmail.com>
2025-01-04 19:48:50 -0500

    built navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import Homepage from "./pages/Homepage"
+import Navbar from "./components/Navbar"
+
 function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Navbar/>
+      <Routes>
+        <Route index element={<Homepage/>} />
+      </Routes>
     </>
   )
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../assets/elite_logo.png'
+
+
+const Navbar = () => {
+  return (
+    <header className='bg-gradient-to-r from-gray-800 via-gray-900 to-black py-4 px-8 flex justify-between items-center'>
+        <div className="flex items-center space-x-3 text-white text-xl">
+            <img src={Logo} alt="logo" width={50} height={50}/>
+            <span className="font-bold text-2xl bg-gradient-to-r from-red-500 to-indigo-500 bg-clip-text text-transparent">
+                Devchronicles
+            </span>
+        </div>
+        <nav>
+            <ul className='flex space-x-6'>
+                <li>
+                    <Link className='text-white'>
+                        Posts
+                    </Link>
+                </li>
+                <li>
+                    <Link className='text-white'>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
